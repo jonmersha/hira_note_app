7684cacbca701c46c6a0649685a64f14f5a56c11
jonmersha <jonmersa@gmail.com>
2020-03-31 00:09:11 +0300

    Schema Correction done

---
+++ app/src/main/java/com/hira/hiranoteapp/ReadNote.java
@@ -0,0 +1,13 @@
+package com.hira.hiranoteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReadNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_note);
+    }
+}
